4e0a86a6e11012baf7bfa3b3f9f03330b0859df9
Deva938 <devajabbaar@gmail.com>
2026-08-05 08:15:22 +0700

    Add files via upload

---
+++ program_pertamaku.py
@@ -0,0 +1,22 @@
+# Program pertama saya
+# Nmama: [Muhammad Deva Jabbaar Riyanto] 
+# Kelas: X RPL 2
+
+print ('================================')
+print ('    SELAMAT DATANG DI PYTHON!') 
+print ('================================')
+print ()
+print ('Progam ini dibuat oleh: ', ' [muhammad deva jabbaar riyanto]')
+print ('Sekolah: SMK TJP Tuban')                                        
+print ('Tahun :', 2024+2)
+print ()
+print (10 + 5)
+print (20 - 8)
+print (4 * 7)
+print (15 / 4)
+print (15 // 4)
+print (15 % 4)
+print (2 ** 8)
+a = 24
+b = 7
+print ('Hasil dari a + b =', a + b)
