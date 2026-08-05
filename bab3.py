84861c2ee141bc28e55f582b6e6fe5f1c9b6f943
Deva938 <devajabbaar@gmail.com>
2026-08-05 12:39:00 +0700

    Add files via upload

---
+++ bab3.py
@@ -0,0 +1,19 @@
+nama = 'deva'
+umur = 16
+tinggi = 150
+lulus = True
+
+print ('Nama saya adalah:', nama)
+print ('Umur saya adalah:', umur, 'tahun')
+print ('Tinggi saya adalah:', tinggi, 'cm')
+print ('Saya sudah lulus:', lulus)
+
+nama = 'sepatu lari'
+stok = 15
+harga = 249.5
+tersedia = True
+print ()
+print ('Nama Produk :', nama)
+print ('Stok Tersedia :', stok)
+print ('Harga ($):', harga)
+print ('Tersedia di Toko:', tersedia)
